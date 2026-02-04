4d692b49e8880a9889b0e3fe57e3f54d44ffc431
Mevlüt Semih <ekenmevlutsemih@gmail.com>
2026-02-04 15:28:33 +0300

    Time: 0 ms (100%), Space: 8.1 MB (65.25%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.c
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.c
@@ -2,10 +2,9 @@ int largestAltitude(int* gain, int gainSize) {
     int max = 0, biker = 0;
     for(int i = 0; i < gainSize; i++){
         biker += gain[i];
-        if(gain[i] > 0){
+        
             if(max < biker){
                 max = biker;
-            }
         }
     }
     return max;
